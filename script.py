726c8ebee450bf99b14668595cff4bacb42d7601
Małgorzata Galińska <malgosia_biegala@o2.pl>
2022-10-10 17:44:21 +0200

    Write to csv

--- script.py
+++ script.py
@@ -1,47 +1,48 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 # coding: utf-8
 
 from logging import error
 import os
 import string
 import sys
+import tokenize
+import csv
+
+statsDictionary = {}
 
 def main (path:string):
     if not os.path.isdir(path):
         error("That is not dir path.")
         return 1
-
-    f = open('FileData.txt', 'w', -1, 'utf-8')
-
+    
     n_of_py = 0
     n_of_others = 0
 
-    for path, subfiles, files in os.walk(path):
-        # f.write('=' * 70 + '\n')
-        # f.write(path + '\n')
-        # if len(subfiles):
-            # f.write('=' * 70 + '\n')
-            # f.write('Podkatalogi:\n' + '=' * 70 + '\n')
-            # for i in subfiles:
-            #     f.write('\n' + i)
-            # f.write('\n')
-        if len(files):
-            f.write('=' * 70 + '\n')
-            f.write('Pliki:\n' + '=' * 70 + '\n')
-            for i in files:
-                a  = i.split(".", 1)
-                if len(a) > 1 and a[1] == "py":
-                    f.write('\n' + i)
-                    n_of_py += 1
+    with open('StatsFile.csv', 'w', -1, 'utf-8') as statsFile:
+        for path, subfiles, files in os.walk(path):
+            for fileName in files:
+                x  = fileName.split(".", 1)
+                if len(x) > 1 and x[1] == "py":
+                    add_to_statistics(path + "/" + fileName)
+                    n_of_py += 1 
                 else:
-                    f.write('\n' + "No Python file!")
                     n_of_others += 1
-            f.write('\n')
-
-    n = n_of_others + n_of_py
-    f.write("\n\nThere is " + str(n_of_py) + " Python files of " + str(n) + " of all files.\n" )
-    f.close()
+        n = n_of_others + n_of_py
+        print("There is " + str(n_of_py) + " Python files of " + str(n) + " of all files.\n\n")
+        csvwriter = csv.writer(statsFile)
+        for key, value in statsDictionary.items():
+            csvwriter.writerow([key, value])
     return 0
+
+def add_to_statistics(fileName:string):
+    with tokenize.open(fileName) as file:
+        tokens = tokenize.generate_tokens(file.readline)
+        for tokenType, tokenString, _, _, _ in tokens:
+            if tokenType == 1:
+                if tokenString in statsDictionary:
+                    statsDictionary[tokenString] += 1
+                else:
+                    statsDictionary[tokenString] = 1
 
 
 if __name__ == '__main__':
